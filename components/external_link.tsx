09a22d4efa1eddb70eb56ad0c2fc89fc721efeb5
Maciej Bernatowicz <krrzeslaw@gmail.com>
2023-05-17 01:10:42 +0200

    Created external link svg

---
+++ components/external_link.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ExternalLink = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M11 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-5m-7 1L20 4m-5 0h5v5"
+      ></path>
+    </svg>
+  );
+};
+
+export default ExternalLink;
